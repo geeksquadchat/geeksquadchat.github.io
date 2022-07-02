480d664101f85e297b70e2075fc53e6bd817b70c
geeksquadchat <108578165+geeksquadchat@users.noreply.github.com>
2022-07-02 18:23:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,108 @@
+<main id="content" class="neve-main" role="main">
 
+		<div data-elementor-type="wp-page" data-elementor-id="5" class="elementor elementor-5">
+									<section class="elementor-section elementor-top-section elementor-element elementor-element-4c7b57ef elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="4c7b57ef" data-element_type="section">
+						<div class="elementor-container elementor-column-gap-default">
+					<div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-54e41a77" data-id="54e41a77" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-353419a9 elementor-widget elementor-widget-image" data-id="353419a9" data-element_type="widget" data-widget_type="image.default">
+				<div class="elementor-widget-container">
+															<img width="508" height="407" src="https://geeksuptech.org/wp-content/uploads/2022/04/call_women.png" class="attachment-large size-large" alt="" loading="lazy" srcset="https://geeksuptech.org/wp-content/uploads/2022/04/call_women.png  508w,https://geeksuptech.org/wp-content/uploads/2022/04/call_women-300x240.png  300w" sizes="(max-width: 508px) 100vw, 508px">															</div>
+				</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-363baa18" data-id="363baa18" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-6ab92203 elementor-widget elementor-widget-heading" data-id="6ab92203" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<h2 class="elementor-heading-title elementor-size-default">Get help with your tech problems.
+</h2>		</div>
+				</div>
+				<div class="elementor-element elementor-element-350e2b68 elementor-widget elementor-widget-text-editor" data-id="350e2b68" data-element_type="widget" data-widget_type="text-editor.default">
+				<div class="elementor-widget-container">
+							Support Plans include Protection, Total Tech Support, 24 x 7 Support. By continuing, you agree to the following terms: Conditions of Use | Privacy | CA Privacy
+
+						</div>
+				</div>
+				<div class="elementor-element elementor-element-595874c6 elementor-widget elementor-widget-button" data-id="595874c6" data-element_type="widget" data-widget_type="button.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-button-wrapper">
+			<a href="javascript:void(Tawk_API.toggle())" class="elementor-button-link elementor-button elementor-size-sm" role="button">
+						<span class="elementor-button-content-wrapper">
+						<span class="elementor-button-text">CHAT NOW</span>
+		</span>
+					</a>
+		</div>
+				</div>
+				</div>
+					</div>
+		</div>
+							</div>
+		</section>
+				<section class="elementor-section elementor-top-section elementor-element elementor-element-47368c6f elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="47368c6f" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+						<div class="elementor-container elementor-column-gap-default">
+					<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-783e3922" data-id="783e3922" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-6d024ebe elementor-widget elementor-widget-spacer" data-id="6d024ebe" data-element_type="widget" data-widget_type="spacer.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-spacer">
+			<div class="elementor-spacer-inner"></div>
+		</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-7c35dece elementor-widget elementor-widget-heading" data-id="7c35dece" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<h2 class="elementor-heading-title elementor-size-default">Schedule a service.
+</h2>		</div>
+				</div>
+				<div class="elementor-element elementor-element-339507f1 elementor-widget elementor-widget-heading" data-id="339507f1" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<div class="elementor-heading-title elementor-size-default">Book your appointment for repair, installation, troubleshooting, or device pickup.
+
+</div>		</div>
+				</div>
+				<div class="elementor-element elementor-element-18056917 elementor-widget elementor-widget-image" data-id="18056917" data-element_type="widget" data-widget_type="image.default">
+				<div class="elementor-widget-container">
+															<img width="1024" height="432" src="https://geeksuptech.org/wp-content/uploads/2022/04/Screenshot-2022-04-22-at-6.19.40-PM-min-1024x432.png" class="attachment-large size-large" alt="" loading="lazy" srcset="https://geeksuptech.org/wp-content/uploads/2022/04/Screenshot-2022-04-22-at-6.19.40-PM-min-1024x432.png  1024w,https://geeksuptech.org/wp-content/uploads/2022/04/Screenshot-2022-04-22-at-6.19.40-PM-min-300x127.png  300w,https://geeksuptech.org/wp-content/uploads/2022/04/Screenshot-2022-04-22-at-6.19.40-PM-min-768x324.png  768w,https://geeksuptech.org/wp-content/uploads/2022/04/Screenshot-2022-04-22-at-6.19.40-PM-min-1536x649.png  1536w,https://geeksuptech.org/wp-content/uploads/2022/04/Screenshot-2022-04-22-at-6.19.40-PM-min-2048x865.png  2048w" sizes="(max-width: 1024px) 100vw, 1024px">															</div>
+				</div>
+					</div>
+		</div>
+							</div>
+		</section>
+				<section class="elementor-section elementor-top-section elementor-element elementor-element-514e6402 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="514e6402" data-element_type="section">
+						<div class="elementor-container elementor-column-gap-default">
+					<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-6c38b9e3" data-id="6c38b9e3" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-190e8bd6 elementor-widget elementor-widget-heading" data-id="190e8bd6" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<h2 class="elementor-heading-title elementor-size-default">About Us
+</h2>		</div>
+				</div>
+				<div class="elementor-element elementor-element-5ef8b5a7 elementor-widget elementor-widget-text-editor" data-id="5ef8b5a7" data-element_type="widget" data-widget_type="text-editor.default">
+				<div class="elementor-widget-container">
+							The Geek Care is the leading remote tech support service. Our Company is founded with the mission to be perfect, professional and responsible towards the customers regarding our services. We are endowed with these three traits. Perfection is what we always aim to achieve. Our product-centric vision is customers’ satisfaction. With the aim to be professionals to bring in our experts, we pertain to reach the high standards in an innovative approach. We are responsible, accountable and amenable for the claims. We bring in a degree of personal accountability on the part of our concerned customers.
+
+						</div>
+				</div>
+					</div>
+		</div>
+							</div>
+		</section>
+				<section class="elementor-section elementor-top-section elementor-element elementor-element-181da7e elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="181da7e" data-element_type="section">
+						<div class="elementor-container elementor-column-gap-default">
+					<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-863e0e0" data-id="863e0e0" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-db4c16d elementor-widget elementor-widget-spacer" data-id="db4c16d" data-element_type="widget" data-widget_type="spacer.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-spacer">
+			<div class="elementor-spacer-inner"></div>
+		</div>
+				</div>
+				</div>
+					</div>
+		</div>
+							</div>
+		</section>
+							</div>
+		
+</main>
